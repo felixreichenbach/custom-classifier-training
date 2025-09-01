78ba12ba30d800219037e8327ffad6e8d8ef3f59
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2025-09-01 13:10:37 +0200

    tweaked script (#7)
    * tweaked script
    
    * tests
    
    * linter
    
    * arg fix

--- tests/test_file_exists.py
+++ tests/test_file_exists.py
@@ -1,6 +1,27 @@
-import os
+import model.training
 
 
-def test_file_exists():
-    assert os.path.exists("labels.txt")
-    assert os.path.exists("classification_model.tflite")
+def test_parse_args():
+    args = [
+        "--dataset_file=./test_dataset/test_data.jsonl",
+        "--model_output_directory=test_output/",
+        "--num_epochs=5",
+        "--labels='OK NOK'",
+        "--model_type=single_label",
+    ]
+
+    data_json, model_dir, num_epochs, labels, model_type = model.training.parse_args(
+        args
+    )
+
+    assert (
+        data_json == "./test_dataset/test_data.jsonl"
+    ), f"Expected dataset_file to be './test_dataset/test_data.jsonl', got {data_json}"
+    assert (
+        model_dir == "test_output/"
+    ), f"Expected model_output_directory to be 'test_output/', got {model_dir}"
+    assert num_epochs == 5, f"Expected num_epochs to be 5, got {num_epochs}"
+    assert labels == "'OK NOK'", f"Expected labels to be 'OK NOK', got {labels}"
+    assert (
+        model_type == "single_label"
+    ), f"Expected model_type to be single_label, got {model_type}"
--- tests/test_parse_args.py
+++ tests/test_parse_args.py
@@ -3,11 +3,11 @@ import model.training
 
 def test_parse_args():
     args = [
-        "--dataset_file=test_data.json",
-        "--model_dir=test_output/",
+        "--dataset_file=./test_dataset/test_data.jsonl",
+        "--model_output_directory=test_output/",
         "--num_epochs=5",
-        "--labels='label_1 label_2'",
-        "--model_type=multi_label",
+        "--labels='OK NOK'",
+        "--model_type=single_label",
     ]
 
     data_json, model_dir, num_epochs, labels, model_type = model.training.parse_args(
@@ -15,15 +15,13 @@ def test_parse_args():
     )
 
     assert (
-        data_json == "test_data.json"
-    ), f"Expected dataset_file to be 'test_data.json', got {data_json}"
+        data_json == "./test_dataset/test_data.jsonl"
+    ), f"Expected dataset_file to be './test_dataset/test_data.jsonl', got {data_json}"
     assert (
         model_dir == "test_output/"
     ), f"Expected model_output_directory to be 'test_output/', got {model_dir}"
     assert num_epochs == 5, f"Expected num_epochs to be 5, got {num_epochs}"
+    assert labels == "'OK NOK'", f"Expected labels to be 'OK NOK', got {labels}"
     assert (
-        labels == "'label_1 label_2'"
-    ), f"Expected labels to be 'label_1 label_2', got {labels}"
-    assert (
-        model_type == "multi_label"
-    ), f"Expected model_type to be multi_label, got {model_type}"
+        model_type == "single_label"
+    ), f"Expected model_type to be single_label, got {model_type}"
