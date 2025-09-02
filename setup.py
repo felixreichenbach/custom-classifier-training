5cdac32a84dbfb7cdc90c5c529b5f9ac3debbe74
felixreichenbach <felix.reichenbach@viam.com>
2025-09-02 16:12:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     install_requires=[
         "google-cloud-aiplatform",
         "google-cloud-storage",
-        "keras==2.14.0",
+        "keras==2.13.1rc0",
         "keras-cv==0.6.4",
         "Keras-Preprocessing==1.1.2",
         "tflite-support",
